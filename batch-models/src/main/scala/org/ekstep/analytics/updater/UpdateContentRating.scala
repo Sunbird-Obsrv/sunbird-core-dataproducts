f0a30548e5ed6179c3e2b22ba38224f8275e1de9
Manjunath Davanam <manjunathd@ilimi.in>
2020-02-07 10:47:18 +0530

    Issue SB-17458 fix: content consumption job

--- batch-models/src/main/scala/org/ekstep/analytics/updater/UpdateContentRating.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/updater/UpdateContentRating.scala
@@ -9,70 +9,141 @@ import org.ekstep.analytics.framework.conf.AppConf
 import org.joda.time.DateTime
 import org.ekstep.analytics.framework.util.{HTTPClient, JSONUtils, JobLogger, RestUtil}
 
-case class ContentRatingResult(contentId: String, numOfTimesRated: Long, averageRating: Double) extends AlgoOutput with Output
 case class Response(id: String, ver: String, ts: String, params: Params, responseCode: String, result: Map[String, AnyRef])
 
-object UpdateContentRating  extends IBatchModelTemplate[Empty, Empty, ContentRatingResult, ContentRatingResult] with Serializable {
+case class ContentMetrics(
+                           contentId: String,
+                           totalRatingsCount: Long,
+                           averageRating: Double,
+                           totalTimeSpentInApp: Long,
+                           totalTimeSpentInPortal: Long,
+                           totalTimeSpentInDeskTop: Long,
+                           totalPlaySessionCountInApp: Long,
+                           totalPlaySessionCountInPortal: Long,
+                           totalPlaySessionCountInDeskTop: Long
+                         ) extends AlgoOutput with Output
+
+object UpdateContentRating extends IBatchModelTemplate[Empty, Empty, ContentMetrics, ContentMetrics] with Serializable {
 
   implicit val className = "org.ekstep.analytics.updater.UpdateContentRating"
+
   override def name: String = "UpdateContentRating"
 
   override def preProcess(data: RDD[Empty], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[Empty] = {
-      data
+    data
   }
 
-  override def algorithm(data: RDD[Empty], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[ContentRatingResult] = {
+  override def algorithm(data: RDD[Empty], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[ContentMetrics] = {
 
-      val contentList = getRatedContents(config, RestUtil)
-      val contentRatingList = getContentRatings(RestUtil)
-      val finalList = contentRatingList.filter(f => contentList.contains(f.contentId))
-      JobLogger.log("content-ids: " + contentList.size + " rating list: " + contentRatingList.size + " filtered list: " + finalList.size, None, Level.INFO)
-      sc.parallelize(finalList)
+    val contentList = getRatedContents(config, RestUtil)
+    val contentRatingList = getContentMetrics(RestUtil, AppConf.getConfig("druid.content.rating.query"))
+    val contentSummaryList = getContentMetrics(RestUtil, AppConf.getConfig("druid.content.summary.query"))
+
+    val finalContentRating = contentRatingList.filter(f => contentList.contains(f.contentId)).map { f =>
+      (f.contentId, f)
+    }
+    val finalContentSummaryList = contentSummaryList.filter(f => contentList.contains(f.contentId)).map { f =>
+      (f.contentId, f)
+    }
+    finalContentRating.join(finalContentSummaryList).map { f =>
+      val ratingData = f._2._1
+      val consumptionData = f._2._2
+      ContentMetrics(f._1,
+        ratingData.totalRatingsCount,
+        ratingData.averageRating,
+        consumptionData.totalTimeSpentInApp,
+        consumptionData.totalTimeSpentInPortal,
+        consumptionData.totalTimeSpentInDeskTop,
+        consumptionData.totalPlaySessionCountInApp,
+        consumptionData.totalPlaySessionCountInPortal,
+        consumptionData.totalPlaySessionCountInDeskTop
+      )
+    }
+    //JobLogger.log("content-ids: " + contentList.size + " rating list: " + contentRatingList.size + " filtered list: " + finalList.size, None, Level.INFO)
+
   }
 
-  override def postProcess(data: RDD[ContentRatingResult], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[ContentRatingResult] = {
-      val baseURL = AppConf.getConfig("lp.system.update.base.url")
-      if (data.count() > 0) {
-          data.foreach { f =>
-              val response = publishRatingToContentModel(f.contentId, f.numOfTimesRated, f.averageRating, baseURL, RestUtil)
-              val msg = response.result.getOrElse("messages", List()).asInstanceOf[List[String]].mkString(",")
-              JobLogger.log("System Update API request for " + f.contentId + " is " + response.params.status.getOrElse(""), Option(Map("error" -> response.params.errmsg.getOrElse(""), "error_msg" -> msg)), Level.INFO)
-          }
+  override def postProcess(data: RDD[ContentMetrics], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[ContentMetrics] = {
+    val baseURL = AppConf.getConfig("lp.system.update.base.url")
+    if (data.count() > 0) {
+      data.foreach { f =>
+        val response = publishMetricsToContentModel(
+          ContentMetrics(
+            f.contentId,
+            f.totalRatingsCount,
+            f.averageRating,
+            f.totalTimeSpentInApp,
+            f.totalTimeSpentInPortal,
+            f.totalTimeSpentInDeskTop,
+            f.totalPlaySessionCountInApp,
+            f.totalPlaySessionCountInPortal,
+            f.totalPlaySessionCountInDeskTop
+          ),
+          baseURL,
+          RestUtil)
+        val msg = response.result.getOrElse("messages", List()).asInstanceOf[List[String]].mkString(",")
+        JobLogger.log("System Update API request for " + f.contentId + " is " + response.params.status.getOrElse(""), Option(Map("error" -> response.params.errmsg.getOrElse(""), "error_msg" -> msg)), Level.INFO)
       }
-      data
+    }
+    data
   }
 
   def getRatedContents(config: Map[String, AnyRef], restUtil: HTTPClient): List[String] = {
-      val apiURL = AppConf.getConfig("druid.sql.host")
-      val startDate = config.getOrElse("startDate", new DateTime().minusDays(1).toString("yyyy-MM-dd")).asInstanceOf[String]
-      var endDate = config.getOrElse("endDate", new DateTime().toString("yyyy-MM-dd")).asInstanceOf[String]
-      if(startDate.equals(endDate)) endDate = new DateTime(endDate).plusDays(1).toString("yyyy-MM-dd")
-      val contentRequest = AppConf.getConfig("druid.unique.content.query").format(new DateTime(startDate).withTimeAtStartOfDay().toString("yyyy-MM-dd HH:mm:ss"), new DateTime(endDate).withTimeAtStartOfDay().toString("yyyy-MM-dd HH:mm:ss"))
-      val contentResponse = restUtil.post[List[Map[String, AnyRef]]](apiURL, contentRequest)
-      contentResponse.map(x => x.getOrElse("Id", "").toString)
+    val apiURL = AppConf.getConfig("druid.sql.host")
+    val startDate = config.getOrElse("startDate", new DateTime().minusDays(1).toString("yyyy-MM-dd")).asInstanceOf[String]
+    var endDate = config.getOrElse("endDate", new DateTime().toString("yyyy-MM-dd")).asInstanceOf[String]
+    if (startDate.equals(endDate)) endDate = new DateTime(endDate).plusDays(1).toString("yyyy-MM-dd")
+    val contentRequest = AppConf.getConfig("druid.unique.content.query").format(new DateTime(startDate).withTimeAtStartOfDay().toString("yyyy-MM-dd HH:mm:ss"), new DateTime(endDate).withTimeAtStartOfDay().toString("yyyy-MM-dd HH:mm:ss"))
+    val contentResponse = restUtil.post[List[Map[String, AnyRef]]](apiURL, contentRequest)
+    contentResponse.map(x => x.getOrElse("Id", "").toString)
   }
 
-  def getContentRatings(restUtil: HTTPClient): List[ContentRatingResult] = {
-      val apiURL = AppConf.getConfig("druid.sql.host")
-      val ratingRequest = AppConf.getConfig("druid.content.rating.query")
-      val contentRatingResponse = restUtil.post[List[Map[String, AnyRef]]](apiURL, ratingRequest)
-      contentRatingResponse.map(x => ContentRatingResult(x.getOrElse("ContentId", "").toString, x.getOrElse("Number of Ratings", 0.0).asInstanceOf[Number].longValue(), x.getOrElse("AverageRating", 0.0).asInstanceOf[Number].doubleValue()))
+  def getContentMetrics(restUtil: HTTPClient, query: String)(implicit sc: SparkContext): RDD[ContentMetrics] = {
+    val apiURL = AppConf.getConfig("druid.sql.host")
+    val response = compute(restUtil.post[List[Map[String, AnyRef]]](apiURL, query))
+    //response.map(x=> ContentMetrics(x.averageRating))
+    sc.parallelize(response.map(x =>
+      ContentMetrics(
+        x.getOrElse("ContentId", "").toString,
+        x.getOrElse("Number of Ratings", 0.0).asInstanceOf[Number].longValue(),
+        x.getOrElse("AverageRating", 0.0).asInstanceOf[Number].doubleValue(),
+        x.getOrElse("totalTimeSpentInApp", 0.0).asInstanceOf[Number].longValue(),
+        x.getOrElse("totalTimeSpentInPortal", 0.0).asInstanceOf[Number].longValue(),
+        x.getOrElse("totalTimeSpentInDeskTop", 0.0).asInstanceOf[Number].longValue(),
+        x.getOrElse("totalPlaySessionCountInApp", 0.0).asInstanceOf[Number].longValue(),
+        x.getOrElse("totalPlaySessionCountInPortal", 0.0).asInstanceOf[Number].longValue(),
+        x.getOrElse("totalPlaySessionCountInDeskTop", 0.0).asInstanceOf[Number].longValue()
+      )))
   }
 
-  def publishRatingToContentModel(contentId: String, numOfTimesRated: Long, avgRating: Double, baseURL: String, restUtil: HTTPClient): Response = {
-      val systemUpdateURL = s"$baseURL/$contentId"
-      val request =
-          s"""
-             |{
-             |  "request": {
-             |    "content": {
-             |      "me_totalRatingsCount": $numOfTimesRated,
-             |      "me_averageRating": $avgRating
-             |    }
-             |  }
-             |}
+  def compute(response: List[Map[String, AnyRef]]): List[Map[String, AnyRef]] = {
+    var listData: List[Map[String, AnyRef]] = List()
+
+
+
+    listData
+  }
+
+  def publishMetricsToContentModel(contentMetrics: ContentMetrics, baseURL: String, restUtil: HTTPClient): Response = {
+    val systemUpdateURL = baseURL + "/" + contentMetrics.contentId
+    val request =
+      s"""
+         |{
+         |  "request": {
+         |    "content": {
+         |      "me_totalRatingsCount": ${contentMetrics.totalRatingsCount},
+         |      "me_averageRating": ${contentMetrics.averageRating},
+         |      "me_total_time_spent_in_app":${contentMetrics.totalPlaySessionCountInApp},
+         |      "me_total_time_spent_in_portal":${contentMetrics.totalTimeSpentInPortal},
+         |      "me_total_time_spent_in_desktop":${contentMetrics.totalTimeSpentInDeskTop},
+         |      "me_total_plays_session_count_in_app":${contentMetrics.totalPlaySessionCountInApp},
+         |      "me_total_play_session_count_in_portal":${contentMetrics.totalPlaySessionCountInDeskTop},
+         |      "me_total_play_session_count_in_desktop":${contentMetrics.totalPlaySessionCountInPortal},
+         |    }
+         |  }
+         |}
                """.stripMargin
-      val response = restUtil.patch[String](systemUpdateURL, request)
-      JSONUtils.deserialize[Response](response)
+    val response = restUtil.patch[String](systemUpdateURL, request)
+    JSONUtils.deserialize[Response](response)
   }
 }
--- batch-models/src/test/scala/org/ekstep/analytics/updater/TestUpdateContentRating.scala
+++ batch-models/src/test/scala/org/ekstep/analytics/updater/TestUpdateContentRating.scala
@@ -6,149 +6,155 @@ import org.joda.time.DateTime
 import org.scalamock.scalatest.MockFactory
 import org.ekstep.analytics.framework.FrameworkContext
 
-class TestUpdateContentRating  extends SparkSpec(null) with MockFactory {
+class TestUpdateContentRating extends SparkSpec(null) with MockFactory {
 
-    implicit val fc = new FrameworkContext();
-    "UpdateContentRating" should "get content list which are rated in given time" in {
+  implicit val fc = new FrameworkContext();
+  "UpdateContentRating" should "get content list which are rated in given time" in {
 
-        val startDate = new DateTime().minusDays(1).toString("yyyy-MM-dd")
-        val endDate = new DateTime().toString("yyyy-MM-dd")
-        val mockRestUtil = mock[HTTPClient]
-        (mockRestUtil.post[List[Map[String, AnyRef]]](_: String, _: String, _: Option[Map[String, String]])(_: Manifest[List[Map[String, AnyRef]]]))
-          .expects("http://localhost:8082/druid/v2/sql/", "{\"query\":\"SELECT DISTINCT \\\"object_id\\\" AS \\\"Id\\\"\\nFROM \\\"druid\\\".\\\"%s\\\" WHERE \\\"eid\\\" = 'FEEDBACK' AND \\\"__time\\\" BETWEEN TIMESTAMP '%s' AND TIMESTAMP '%s' \"}".format("telemetry-events", new DateTime(startDate).withTimeAtStartOfDay().toString("yyyy-MM-dd HH:mm:ss"), new DateTime(endDate).withTimeAtStartOfDay().toString("yyyy-MM-dd HH:mm:ss")), None, manifest[List[Map[String, AnyRef]]])
-          .returns(List(Map("ContentId" -> "test-1"), Map("ContentId" -> "test-2")))
+    val startDate = new DateTime().minusDays(1).toString("yyyy-MM-dd")
+    val endDate = new DateTime().toString("yyyy-MM-dd")
+    val mockRestUtil = mock[HTTPClient]
+    (mockRestUtil.post[List[Map[String, AnyRef]]](_: String, _: String, _: Option[Map[String, String]])(_: Manifest[List[Map[String, AnyRef]]]))
+      .expects("http://localhost:8082/druid/v2/sql/", "{\"query\":\"SELECT DISTINCT \\\"object_id\\\" AS \\\"Id\\\"\\nFROM \\\"druid\\\".\\\"%s\\\" WHERE \\\"eid\\\" = 'FEEDBACK' AND \\\"__time\\\" BETWEEN TIMESTAMP '%s' AND TIMESTAMP '%s' \"}".format("telemetry-events", new DateTime(startDate).withTimeAtStartOfDay().toString("yyyy-MM-dd HH:mm:ss"), new DateTime(endDate).withTimeAtStartOfDay().toString("yyyy-MM-dd HH:mm:ss")), None, manifest[List[Map[String, AnyRef]]])
+      .returns(List(Map("ContentId" -> "test-1"), Map("ContentId" -> "test-2")))
 
-        val contentIds = UpdateContentRating.getRatedContents(Map("startDate" -> startDate.asInstanceOf[AnyRef], "endDate" -> endDate.asInstanceOf[AnyRef]), mockRestUtil)
-        contentIds.size should be(2)
+    val contentIds = UpdateContentRating.getRatedContents(Map("startDate" -> startDate.asInstanceOf[AnyRef], "endDate" -> endDate.asInstanceOf[AnyRef]), mockRestUtil)
+    contentIds.size should be(2)
+  }
+
+  it should "alter end date for replay scenario" in {
+
+    val startDate = new DateTime().minusDays(1).toString("yyyy-MM-dd")
+    val endDate = new DateTime().toString("yyyy-MM-dd")
+    val mockRestUtil = mock[HTTPClient]
+    (mockRestUtil.post[List[Map[String, AnyRef]]](_: String, _: String, _: Option[Map[String, String]])(_: Manifest[List[Map[String, AnyRef]]]))
+      .expects("http://localhost:8082/druid/v2/sql/", "{\"query\":\"SELECT DISTINCT \\\"object_id\\\" AS \\\"Id\\\"\\nFROM \\\"druid\\\".\\\"%s\\\" WHERE \\\"eid\\\" = 'FEEDBACK' AND \\\"__time\\\" BETWEEN TIMESTAMP '%s' AND TIMESTAMP '%s' \"}".format("telemetry-events", new DateTime(startDate).withTimeAtStartOfDay().toString("yyyy-MM-dd HH:mm:ss"), new DateTime(endDate).withTimeAtStartOfDay().toString("yyyy-MM-dd HH:mm:ss")), None, manifest[List[Map[String, AnyRef]]])
+      .returns(List(Map("ContentId" -> "test-1"), Map("ContentId" -> "test-2")))
+
+    val contentIds = UpdateContentRating.getRatedContents(Map("startDate" -> startDate.asInstanceOf[AnyRef], "endDate" -> startDate.asInstanceOf[AnyRef]), mockRestUtil)
+    contentIds.size should be(2)
+  }
+
+  it should "get all content ratings" in {
+
+    val mockRestUtil = mock[HTTPClient]
+    (mockRestUtil.post[List[Map[String, AnyRef]]](_: String, _: String, _: Option[Map[String, String]])(_: Manifest[List[Map[String, AnyRef]]]))
+      .expects("http://localhost:8082/druid/v2/sql/", "{\"query\": \"SELECT \\\"object_id\\\" AS ContentId, COUNT(*) AS \\\"Number of Ratings\\\", SUM(edata_rating) AS \\\"Total Ratings\\\", SUM(edata_rating)/COUNT(*) AS \\\"AverageRating\\\" FROM \\\"druid\\\".\\\"telemetry-events\\\" WHERE \\\"eid\\\" = 'FEEDBACK' GROUP BY \\\"object_id\\\"\"}", None, manifest[List[Map[String, AnyRef]]])
+      .returns(List(Map("ContentId" -> "test-1", "Total Ratings" -> 25.asInstanceOf[AnyRef], "Number of Ratings" -> 5.asInstanceOf[AnyRef], "AverageRating" -> 5.asInstanceOf[AnyRef]), Map("ContentId" -> "test-2", "AverageRating" -> 3.asInstanceOf[AnyRef])))
+
+    val contentRatings = UpdateContentRating.getContentMetrics(mockRestUtil, null)
+    contentRatings.map {
+      f => f.averageRating should be()
     }
+    //        contentRatings.map{f=>
+    //            contentRatings.size should be(2)
+    //            contentRatings(0).contentId should be("test-1")
+    //            contentRatings(0).averageRating should be(5)
+    //            contentRatings(0).numOfTimesRated should be(5)
+    //            contentRatings(1).contentId should be("test-2")
+    //            contentRatings(1).averageRating should be(3)
+    //            contentRatings(1).numOfTimesRated should be(0)
+    //        }
 
-    it should "alter end date for replay scenario" in {
+  }
 
-        val startDate = new DateTime().minusDays(1).toString("yyyy-MM-dd")
-        val endDate = new DateTime().toString("yyyy-MM-dd")
-        val mockRestUtil = mock[HTTPClient]
-        (mockRestUtil.post[List[Map[String, AnyRef]]](_: String, _: String, _: Option[Map[String, String]])(_: Manifest[List[Map[String, AnyRef]]]))
-          .expects("http://localhost:8082/druid/v2/sql/", "{\"query\":\"SELECT DISTINCT \\\"object_id\\\" AS \\\"Id\\\"\\nFROM \\\"druid\\\".\\\"%s\\\" WHERE \\\"eid\\\" = 'FEEDBACK' AND \\\"__time\\\" BETWEEN TIMESTAMP '%s' AND TIMESTAMP '%s' \"}".format("telemetry-events", new DateTime(startDate).withTimeAtStartOfDay().toString("yyyy-MM-dd HH:mm:ss"), new DateTime(endDate).withTimeAtStartOfDay().toString("yyyy-MM-dd HH:mm:ss")), None, manifest[List[Map[String, AnyRef]]])
-          .returns(List(Map("ContentId" -> "test-1"), Map("ContentId" -> "test-2")))
+  ignore should "check for system update API call" in {
 
-        val contentIds = UpdateContentRating.getRatedContents(Map("startDate" -> startDate.asInstanceOf[AnyRef], "endDate" -> startDate.asInstanceOf[AnyRef]), mockRestUtil)
-        contentIds.size should be(2)
-    }
-
-    it should "get all content ratings" in {
-
-        val mockRestUtil = mock[HTTPClient]
-        (mockRestUtil.post[List[Map[String, AnyRef]]](_: String, _: String, _: Option[Map[String, String]])(_: Manifest[List[Map[String, AnyRef]]]))
-          .expects("http://localhost:8082/druid/v2/sql/", "{\"query\": \"SELECT \\\"object_id\\\" AS ContentId, COUNT(*) AS \\\"Number of Ratings\\\", SUM(edata_rating) AS \\\"Total Ratings\\\", SUM(edata_rating)/COUNT(*) AS \\\"AverageRating\\\" FROM \\\"druid\\\".\\\"telemetry-events\\\" WHERE \\\"eid\\\" = 'FEEDBACK' GROUP BY \\\"object_id\\\"\"}", None, manifest[List[Map[String, AnyRef]]])
-          .returns(List(Map("ContentId" -> "test-1", "Total Ratings" -> 25.asInstanceOf[AnyRef], "Number of Ratings" -> 5.asInstanceOf[AnyRef], "AverageRating" -> 5.asInstanceOf[AnyRef]), Map("ContentId" -> "test-2", "AverageRating" -> 3.asInstanceOf[AnyRef])))
-
-        val contentRatings = UpdateContentRating.getContentRatings(mockRestUtil)
-        contentRatings.size should be(2)
-        contentRatings(0).contentId should be("test-1")
-        contentRatings(0).averageRating should be(5)
-        contentRatings(0).numOfTimesRated should be(5)
-        contentRatings(1).contentId should be("test-2")
-        contentRatings(1).averageRating should be(3)
-        contentRatings(1).numOfTimesRated should be(0)
-    }
-
-    ignore should "check for system update API call" in {
-
-        val mockRestUtil = mock[HTTPClient]
-        val systemUpdateURL = "http://localhost:8080/learning-service/system/v3/content/update/test-1"
-        val request =
-            s"""
-               |{
-               |  "request": {
-               |    "content": {
-               |      "me_totalRatingsCount": 5,
-               |      "me_averageRating": 5.0
-               |    }
-               |  }
-               |}
+    val mockRestUtil = mock[HTTPClient]
+    val systemUpdateURL = "http://localhost:8080/learning-service/system/v3/content/update/test-1"
+    val request =
+      s"""
+         |{
+         |  "request": {
+         |    "content": {
+         |      "me_totalRatingsCount": 5,
+         |      "me_averageRating": 5.0
+         |    }
+         |  }
+         |}
              """.stripMargin
-        val mockResponse =
-          s"""
-             |{
-             |    "id": "ekstep.learning.system.content.update",
-             |    "ver": "1.0",
-             |    "ts": "2019-05-02T12:20:17ZZ",
-             |    "params": {
-             |        "resmsgid": "622ade80-e22a-4cc1-8683-d002babe9ae6",
-             |        "msgid": null,
-             |        "err": null,
-             |        "status": "successful",
-             |        "errmsg": null
-             |    },
-             |    "responseCode": "OK",
-             |    "result": {
-             |        "node_id": "test-1",
-             |        "versionKey": "1554515533414"
-             |    }
-             |}
+    val mockResponse =
+      s"""
+         |{
+         |    "id": "ekstep.learning.system.content.update",
+         |    "ver": "1.0",
+         |    "ts": "2019-05-02T12:20:17ZZ",
+         |    "params": {
+         |        "resmsgid": "622ade80-e22a-4cc1-8683-d002babe9ae6",
+         |        "msgid": null,
+         |        "err": null,
+         |        "status": "successful",
+         |        "errmsg": null
+         |    },
+         |    "responseCode": "OK",
+         |    "result": {
+         |        "node_id": "test-1",
+         |        "versionKey": "1554515533414"
+         |    }
+         |}
            """.stripMargin
 
-        (mockRestUtil.patch[String](_: String, _: String, _: Option[Map[String, String]])(_: Manifest[String]))
-            .expects(systemUpdateURL, request, None, manifest[String])
-            .returns(mockResponse)
+    (mockRestUtil.patch[String](_: String, _: String, _: Option[Map[String, String]])(_: Manifest[String]))
+      .expects(systemUpdateURL, request, None, manifest[String])
+      .returns(mockResponse)
 
-        val response = UpdateContentRating.publishRatingToContentModel("test-1", 5, 5.0, "http://localhost:8080/learning-service/system/v3/content/update", mockRestUtil)
-        println(response)
-        response.params.status.getOrElse("") should be("successful")
-        response.result.getOrElse("node_id", "") should be("test-1")
+    val response = UpdateContentRating.publishMetricsToContentModel(ContentMetrics("test-1", 100, 20.0, 122, 432, 32432, 33, 53, 552), "http://localhost:8080/learning-service/system/v3/content/update", mockRestUtil)
+    println(response)
+    response.params.status.getOrElse("") should be("successful")
+    response.result.getOrElse("node_id", "") should be("test-1")
 
-    }
+  }
 
-    ignore should "check for system update API call failure" in {
+  ignore should "check for system update API call failure" in {
 
-        val mockRestUtil = mock[HTTPClient]
-        val systemUpdateURL = "http://localhost:8080/learning-service/system/v3/content/update/test-1"
-        val request =
-            s"""
-               |{
-               |  "request": {
-               |    "content": {
-               |      "me_totalRatingsCount": 5,
-               |      "me_averageRating": 5.0
-               |    }
-               |  }
-               |}
+    val mockRestUtil = mock[HTTPClient]
+    val systemUpdateURL = "http://localhost:8080/learning-service/system/v3/content/update/test-1"
+    val request =
+      s"""
+         |{
+         |  "request": {
+         |    "content": {
+         |      "me_totalRatingsCount": 5,
+         |      "me_averageRating": 5.0
+         |    }
+         |  }
+         |}
              """.stripMargin
-        val mockResponse =
-            s"""
-               |{
-               |    "id": "ekstep.learning.system.content.update",
-               |    "ver": "1.0",
-               |    "ts": "2019-05-14T14:43:36ZZ",
-               |    "params": {
-               |        "resmsgid": "c5369a14-eb6e-4eb0-9288-d4cb272d59b9",
-               |        "msgid": null,
-               |        "err": "ERR_GRAPH_UPDATE_NODE_VALIDATION_FAILED",
-               |        "status": "failed",
-               |        "errmsg": "Node Metadata validation failed"
-               |    },
-               |    "responseCode": "CLIENT_ERROR",
-               |    "result": {
-               |        "messages": [
-               |            "Please provide framework.",
-               |            "Metadata contentType should be one of: [Resource, Collection, TextBook, LessonPlan, Course, Template, Asset, Plugin, LessonPlanUnit, CourseUnit, TextBookUnit, TeachingMethod, PedagogyFlow]",
-               |            "Metadata resourceType should be one of: [Read, Learn, Teach, Play, Test, Practice, Experiment, Collection, Book, Lesson Plan, Course, Theory, Worksheet, Practical]"
-               |        ],
-               |        "node_id": "org.ekstep.jun16.story.test05"
-               |    }
-               |}
+    val mockResponse =
+      s"""
+         |{
+         |    "id": "ekstep.learning.system.content.update",
+         |    "ver": "1.0",
+         |    "ts": "2019-05-14T14:43:36ZZ",
+         |    "params": {
+         |        "resmsgid": "c5369a14-eb6e-4eb0-9288-d4cb272d59b9",
+         |        "msgid": null,
+         |        "err": "ERR_GRAPH_UPDATE_NODE_VALIDATION_FAILED",
+         |        "status": "failed",
+         |        "errmsg": "Node Metadata validation failed"
+         |    },
+         |    "responseCode": "CLIENT_ERROR",
+         |    "result": {
+         |        "messages": [
+         |            "Please provide framework.",
+         |            "Metadata contentType should be one of: [Resource, Collection, TextBook, LessonPlan, Course, Template, Asset, Plugin, LessonPlanUnit, CourseUnit, TextBookUnit, TeachingMethod, PedagogyFlow]",
+         |            "Metadata resourceType should be one of: [Read, Learn, Teach, Play, Test, Practice, Experiment, Collection, Book, Lesson Plan, Course, Theory, Worksheet, Practical]"
+         |        ],
+         |        "node_id": "org.ekstep.jun16.story.test05"
+         |    }
+         |}
            """.stripMargin
 
 
-        (mockRestUtil.patch[String](_: String, _: String, _: Option[Map[String, String]])( _: Manifest[String]))
-          .expects(systemUpdateURL, request, None, manifest[String])
-          .returns(mockResponse)
+    (mockRestUtil.patch[String](_: String, _: String, _: Option[Map[String, String]])(_: Manifest[String]))
+      .expects(systemUpdateURL, request, None, manifest[String])
+      .returns(mockResponse)
 
-        val response = UpdateContentRating.publishRatingToContentModel("test-1", 5, 5.0, "http://localhost:8080/learning-service/system/v3/content/update", mockRestUtil)
-        println(response)
-        response.params.status.getOrElse("") should be("failed")
-        response.result.getOrElse("node_id", "") should be("org.ekstep.jun16.story.test05")
+    val response = UpdateContentRating.publishMetricsToContentModel(ContentMetrics("test-1", 100, 20.0, 122, 432, 32432, 33, 53, 552), "http://localhost:8080/learning-service/system/v3/content/update", mockRestUtil)
+    println(response)
+    response.params.status.getOrElse("") should be("failed")
+    response.result.getOrElse("node_id", "") should be("org.ekstep.jun16.story.test05")
 
-    }
+  }
 
 }
