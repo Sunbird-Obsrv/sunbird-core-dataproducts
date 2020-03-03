ddf9badbdc08c2b410934ecf8cdaeb481a6bb3df
Manjunath Davanam <manjunathd@ilimi.in>
2020-03-03 19:05:57 +0530

    Issue SB-17994 fix: Elastic search index issue fix

--- batch-models/src/test/scala/org/ekstep/analytics/job/report/TestCourseMetricsJob.scala
+++ batch-models/src/test/scala/org/ekstep/analytics/job/report/TestCourseMetricsJob.scala
@@ -149,12 +149,16 @@ class TestCourseMetricsJob extends BaseReportSpec with MockFactory {
     CourseMetricsJob.prepareReport(spark, storageConfig, reporterMock.loadData)
 
     // TODO: Add assertions here
-    EmbeddedES.getAllDocuments("cbatchstats-08-07-2018-16-30").foreach(f => {
-      //Console.println(f)
-    })
-    EmbeddedES.getAllDocuments("cbatch").foreach(f => {
-     // Console.println(f)
-    })
+//    EmbeddedES.getAllDocuments(AppConf.getConfig("course.metrics.es.index.cbatch")+"/_doc").foreach(f => {
+//
+//      // Console.println(f)
+//    })
+//
+//    EmbeddedES.getAllDocuments(AppConf.getConfig("course.metrics.es.alias")).foreach(f => {
+//      //Console.println(f)
+//    })
+
+
     
     (new HadoopFileUtil()).delete(spark.sparkContext.hadoopConfiguration, "src/test/resources/course-metrics")
   }
