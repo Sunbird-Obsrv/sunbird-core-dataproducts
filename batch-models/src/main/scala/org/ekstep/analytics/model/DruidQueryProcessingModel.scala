3c8c7f690caf3fc871a0e84a4bb9167f305236a6
SowmyaDixit <sowmyadixit7@gmail.com>
2020-04-01 12:56:33 +0530

    Issue #SB-18525 feat: Remove print statements

--- batch-models/src/main/scala/org/ekstep/analytics/model/DruidQueryProcessingModel.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/model/DruidQueryProcessingModel.scala
@@ -166,9 +166,7 @@ object DruidQueryProcessingModel extends IBatchModelTemplate[DruidOutput, DruidO
       val mergeConf = reportMergeConfig.get
       val reportPath = mergeConf.reportPath
       val filesList = deltaFiles.map{f =>
-        println("delta path: " + f.substring(f.indexOf(storageConfig.fileName, 0)))
         val reportPrefix = f.substring(0, f.lastIndexOf("/")).split(reportId)(1)
-        println("report path: " + reportPrefix + "/" + reportPath)
         Map("reportPath" -> (reportPrefix + "/" + reportPath), "deltaPath" -> f.substring(f.indexOf(storageConfig.fileName, 0)))
       }
       val mergeScriptConfig = MergeScriptConfig(reportId, mergeConf.frequency, mergeConf.basePath, mergeConf.rollup,
