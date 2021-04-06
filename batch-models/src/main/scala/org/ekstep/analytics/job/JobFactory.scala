7f14c7bf13533df6c9bb517c6fcf4fe8ea6c9357
Sowmya N Dixit <sowmyadixit7@gmail.com>
2021-04-06 11:10:08 +0530

    Clean up of unused Data Products code (#113)
    * Issue #TG-723 feat: Clean up of unused Data Products code
    
    * Issue #TG-723 feat: Clean up of unused Data Products code
    
    * Issue #TG-723 feat: Clean up of unused Data Products code
    
    * Issue #TG-723 feat: Clean up of unused Data Products code
    
    * Issue #TG-723 feat: Clean up of unused Data Products code

--- batch-models/src/main/scala/org/ekstep/analytics/job/JobFactory.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/job/JobFactory.scala
@@ -6,13 +6,11 @@ import org.ekstep.analytics.framework.IJob
 import org.ekstep.analytics.framework.exception.JobNotFoundException
 import org.ekstep.analytics.job.batch.VideoStreamingJob
 import org.ekstep.analytics.job.metrics.MetricsAuditJob
-import org.ekstep.analytics.job.summarizer.DeviceSummarizer
 import org.ekstep.analytics.job.summarizer.DruidQueryProcessor
 import org.ekstep.analytics.job.summarizer.ExperimentDefinitionJob
 import org.ekstep.analytics.job.summarizer.MonitorSummarizer
 import org.ekstep.analytics.job.summarizer.WorkFlowSummarizer
 import org.ekstep.analytics.job.updater.ContentRatingUpdater
-import org.ekstep.analytics.job.updater.DeviceProfileUpdater
 
 /**
  * @author Santhosh
@@ -26,10 +24,6 @@ object JobFactory {
         MonitorSummarizer
       case "wfs" =>
         WorkFlowSummarizer
-      case "ds" =>
-        DeviceSummarizer
-      case "dpu" =>
-        DeviceProfileUpdater
       case "video-streaming" =>
         VideoStreamingJob
       case "telemetry-replay" =>
@@ -40,14 +34,6 @@ object JobFactory {
         ContentRatingUpdater
       case "experiment" =>
         ExperimentDefinitionJob
-      case "daily-metrics" =>
-        DruidQueryProcessor
-      case "desktop-consumption-report" =>
-        DruidQueryProcessor
-      case "district-monthly" =>
-        DruidQueryProcessor
-      case "district-weekly" =>
-        DruidQueryProcessor
       case "audit-metrics-report" =>
         MetricsAuditJob
       case "druid_reports" =>
