a4cefb3de9f05d1a91d0c7a8a37c384924817f9e
fathimasherin <fathimasherin362@gmail.com>
2022-03-15 13:55:48 +0530

    fix: added timestamp field and removed the comments

--- batch-models/src/main/scala/org/ekstep/analytics/dashboard/UserCourseProgressModel.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/dashboard/UserCourseProgressModel.scala
@@ -8,29 +8,14 @@ import org.ekstep.analytics.framework.{AlgoInput, AlgoOutput, FrameworkContext, 
 
 case class UserCourseInput() extends AlgoInput
 @scala.beans.BeanInfo
-case class UserCourseOutput(userid: Any, courseid: Any, percentage: Any) extends Output with AlgoOutput
+case class UserCourseOutput(userid: Any, courseid: Any, percentage: Any, timestamp: Long) extends Output with AlgoOutput
 
 object UserCourseProgressModel extends IBatchModelTemplate[String, UserCourseInput, UserCourseOutput, UserCourseOutput]{
-  /**
-   * Pre processing steps before running the algorithm. Few pre-process steps are
-   * 1. Transforming input - Filter/Map etc.
-   * 2. Join/fetch data from LP
-   * 3. Join/Fetch data from Cassandra
-   */
   override def preProcess(events: RDD[String], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[UserCourseInput] = {
     sc.parallelize(Seq())
   }
 
-  /**
-   * Method which runs the actual algorithm
-   */
   override def algorithm(events: RDD[UserCourseInput], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[UserCourseOutput] = {
-//    implicit val spark: SparkSession = SparkSession
-//      .builder()
-//      .appName("UserCourseProgress")
-//      .config("spark.cassandra.connection.host", "10.0.0.7")
-//      .config("spark.cassandra.output.batch.size.rows", "1000")
-//      .getOrCreate()
     implicit val spark: SparkSession = SparkSession.builder.config(sc.getConf).getOrCreate()
     val userCourseData = spark.read.format("org.apache.spark.sql.cassandra")
       .option("inferSchema", "true")
@@ -38,16 +23,10 @@ object UserCourseProgressModel extends IBatchModelTemplate[String, UserCourseInp
       .option("table", "user_content_consumption")
       .load().persist(StorageLevel.MEMORY_ONLY);
     val progressData = userCourseData.select("userid", "courseid", "completionpercentage");
-    val outputData: RDD[UserCourseOutput] = progressData.rdd.map{f => UserCourseOutput(f(0) , f(1), f(2))}
+    val outputData: RDD[UserCourseOutput] = progressData.rdd.map{f => UserCourseOutput(f(0) , f(1), f(2), System.currentTimeMillis())}
     outputData.distinct()
   }
 
-  /**
-   * Post processing on the algorithm output. Some of the post processing steps are
-   * 1. Saving data to Cassandra
-   * 2. Converting to "MeasuredEvent" to be able to dispatch to Kafka or any output dispatcher
-   * 3. Transform into a structure that can be input to another data product
-   */
   override def postProcess(events: RDD[UserCourseOutput], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[UserCourseOutput] = {
     events
   }
