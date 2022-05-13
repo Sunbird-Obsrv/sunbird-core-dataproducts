5e01b1584d9991fee9790991e6361e1d59f3c4bd
sowmya-dixit <sowmyadixit7@gmail.com>
2022-05-13 14:17:38 +0530

    Issue #SB-28238 feat: Upgrade to Spark in Core Data Products: Logger for debug

--- batch-models/src/main/scala/org/ekstep/analytics/job/JobExecutor.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/job/JobExecutor.scala
@@ -5,11 +5,12 @@ import org.ekstep.analytics.framework.FrameworkContext
 /**
  * @author Santhosh
  */
-object JobExecutor {
+object JobExecutor extends App{
 
     val className = "org.ekstep.analytics.job.JobExecutor"
 
     def main(model: String, config: String) {
+        println("inside Job Executor main method")
         implicit val fc : FrameworkContext = null
         val job = JobFactory.getJob(model);
         job.main(config)(None, Option(fc))
